879301a3a8553196ca49d30b752c163134941439
Diego Jacobs <diegojacobs9595@gmail.com>
2017-03-16 17:45:08 -0600

    Door & Window

--- Main.c
+++ Main.c
@@ -46,6 +46,32 @@ void main ()
     roof[2][0] = 300;
     roof[2][1] = 200;
 
+    //Door
+    door[0][0] = 135;
+    door[0][1] = 400;
+
+    door[1][0] = 135;
+    door[1][1] = 300;
+
+    door[2][0] = 195;
+    door[2][1] = 300;
+
+    door[3][0] = 195;
+    door[3][1] = 400;
+
+    //Window
+    window[0][0] = 225;
+    window[0][1] = 275;
+
+    window[1][0] = 225;
+    window[1][1] = 225;
+
+    window[2][0] = 275;
+    window[2][1] = 225;
+
+    window[3][0] = 275;
+    window[3][1] = 275;
+
     //Tree
     tree[0][0] = 500;
     tree[0][1] = 350;
@@ -70,8 +96,10 @@ void main ()
     trunk[3][1] = 400;
 
     drawFilledRectangle(0, 0, 800, 600, 15, 15, 1, 0, 0);
-    scanline(house, 4, 3, 3, 1, 0, 0);
+    scanline(house, 4, 32, 32, 1, 0, 0);
     scanline(roof, 3, 4, 4, 1, 0, 0);
+    scanline(door, 4, 4, 4, 1, 0, 0);
+    scanline(window, 4, 3, 3, 1, 0, 0);
     scanline(tree, 3, 2, 2, 1, 0, 0);
     scanline(trunk, 4, 67, 67, 1, 0, 0);
 
